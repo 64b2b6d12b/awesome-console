338af5ee454d2c41984c34fbd7e67ebede98542a
64b2b6d12b <64b2b6d12b@users.noreply.github.com>
2015-06-23 02:53:09 -0400

    added RVM GPG key

--- tldr.sh
+++ tldr.sh
@@ -2,16 +2,17 @@
 
 yum install -y ncurses-devel
 yum groupinstall -y 'development tools'
+gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 
 curl -OL https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
-tar -xvzf libevent-*.tar.gz
+tar -xzf libevent-*.tar.gz
 cd libevent*
 ./configure
 make
 make install
 
 curl -OL https://github.com/tmux/tmux/releases/download/2.0/tmux-2.0.tar.gz
-tar -xvzf tmux-*.tar.gz
+tar -xzf tmux-*.tar.gz
 cd tmux-*
 ./configure
 make
@@ -23,5 +24,5 @@ curl -L get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm reload
 rvm install 2.1
-yum install rubygems
+yum install -y rubygems
 gem install tmuxinator
