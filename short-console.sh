53da11bad3de0e8065a6ce60f4381fbf6fc97c1d
64b2b6d12b <64b2b6d12b@users.noreply.github.com>
2015-06-24 18:27:00 -0400

    made changes to dir variables

--- short-console.sh
+++ short-console.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-LIBEVENT_PACKAGE=$(curl -s https://api.github.com/repos/libevent/libevent/releases/latest | grep -Eo 'https.*stable.tar.gz' | head -n 1)
-TMUX_PACKAGE=$(curl -s https://api.github.com/repos/tmux/tmux/releases/latest | grep -Eo 'https.*.tar.gz' | head -n 1)
-LIBEVENT_PACKAGE_DIR=$(echo $LIBEVENT_PACKAGE | awk -F/ '{print $NF}')
-TMUX_PACKAGE_DIR=$(echo $TMUX_PACKAGE | awk -F/ '{print $NF}')
+LIBEVENT_URL=$(curl -s https://api.github.com/repos/libevent/libevent/releases/latest | grep -Eo 'https.*stable.tar.gz' | head -n 1)
+TMUX_URL=$(curl -s https://api.github.com/repos/tmux/tmux/releases/latest | grep -Eo 'https.*.tar.gz' | head -n 1)
+LIBEVENT_TAR=$(echo $LIBEVENT_URL | awk -F/ '{print $NF}')
+TMUX_TAR=$(echo $TMUX_URL | awk -F/ '{print $NF}')
+LIBEVENT_DIR=$(echo ${LIBEVENT_TAR%.tar.gz})
+TMUX_DIR=$(echo ${TMUX_TAR%.tar.gz})
 
 function install_yum_dependencies() {
 yum install -y ncurses-devel
@@ -11,18 +13,18 @@ yum groupinstall -y 'development tools'
 }
 
 function install_libevent() {
-curl -OL "$LIBEVENT_PACKAGE"
-tar -xzf libevent-*.tar.gz
-cd $LIBEVENT_PACKAGE_DIR
+curl -OL "$LIBEVENT_URL"
+tar -xzf "$LIBEVENT_TAR"
+cd "$LIBEVENT_DIR"
 ./configure
 make
 make install
 }
 
 function install_tmux() {
-curl -OL "$TMUX_PACKAGE"
-tar -xzf tmux-*.tar.gz
-cd $TMUX_PACKAGE_DIR
+curl -OL "$TMUX_URL"
+tar -xzf "$TMUX_TAR"
+cd "$TMUX_DIR"
 ./configure
 make
 make install
@@ -30,7 +32,7 @@ echo export LD_LIBRARY_PATH=/usr/local/lib >> ~/.bash_profile
 source ~/.bash_profile
 }
 
-function install_rvm-gems-tmuxinator() {
+function install_ruby_tmuxinator() {
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -L get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
